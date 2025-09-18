80cd4de5e01ad2607bd59128ec9dae33b4952ff4
osamanaser <nicolanasir@hotmail.fr>
2025-09-18 18:57:52 +0200

    Added Wallet module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArbitrageOpportunityModule } from './arbitrage-opportunity/arbitrage-opportunity.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [ArbitrageOpportunityModule],
+  imports: [ArbitrageOpportunityModule, WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
